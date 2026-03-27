ddf29b0ca3cbe4ea276ba6337fe221db591573ae
nch-n <nortonchen1999@gmail.com>
2026-03-27 21:05:35 +1100

    add trmnl helper endpoint

--- api/trmnl.js
+++ api/trmnl.js
@@ -1,11 +1,37 @@
-module.exports = function handler(req, res) {
-  const redirect = req.query.redirect_uri;
+module.exports = async function handler(req, res) {
+  const { method, query } = req;
 
-  // If TRMNL expects a redirect ? send it back
-  if (redirect) {
-    return res.redirect(redirect);
+  // ------------------------
+  // INSTALL FLOW
+  // ------------------------
+  if (query.redirect_uri) {
+    const redirectUrl = new URL(query.redirect_uri);
+
+    // fake install data
+    redirectUrl.searchParams.set("external_user_id", "tram-user");
+    redirectUrl.searchParams.set("access_token", "tram-token");
+
+    return res.redirect(redirectUrl.toString());
   }
 
-  // Otherwise just return success
-  return res.status(200).json({ success: true });
+  // ------------------------
+  // WEBHOOK (POST)
+  // ------------------------
+  if (method === "POST") {
+    return res.status(200).json({ success: true });
+  }
+
+  // ------------------------
+  // MANAGEMENT PAGE
+  // ------------------------
+  res.setHeader("Content-Type", "text/html");
+
+  return res.send(`
+    <html>
+      <body style="font-family: sans-serif; padding: 20px;">
+        <h1>?? Tram Tracker Plugin</h1>
+        <p>Your plugin is installed and working.</p>
+      </body>
+    </html>
+  `);
 };
