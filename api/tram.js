29796e0bcaabf573d119a8950ffeb95ef990df19
nch-n <nortonchen1999@gmail.com>
2026-03-27 21:29:34 +1100

    final tram display

--- api/tram.js
+++ api/tram.js
@@ -152,10 +152,9 @@ module.exports = async function handler(req, res) {
     // ?? TRMNL DISPLAY FORMAT
 return res.status(200).json({
   items: trams.map((t, i) => ({
-    title:
-      i === 0
-        ? `?? ${t.line} ${t.destination}`
-        : `${t.line} ${t.destination}`,
+    title: i === 0
+      ? `?? ${t.line}  ${t.destination}`
+      : `${t.line}  ${t.destination}`,
     right: t.eta
   }))
 });
